c397730d73fb82a649b96261af152117be31b4db
jurisdoctor <tomtldr@gmail.com>
2023-11-21 13:49:32 -0800

    base project - reset

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# YouTube Clone?
+# typescript testing
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-const App = () => {
-  return <>YouTube</>;
-};
+import "./App.css";
+
+import React from "react";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>typescript testing</h1>
+    </div>
+  );
+}
 
 export default App;
